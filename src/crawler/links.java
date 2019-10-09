fc471195ce4b0ae6b2ab28ece7cacfd07128076d
Luca Bosse <luca.bosse+github@gmail.com>
2019-10-09 14:43:28 +0200

    changed folder structur

--- src/crawler/links.java
+++ src/crawler/links.java
@@ -1,4 +1,4 @@
-package src;
+package src.crawler;
 
 import java.net.MalformedURLException;
 import java.net.URL;
--- src/crawler/main.java
+++ src/crawler/main.java
@@ -1,4 +1,4 @@
-package src;
+package src.crawler;
 
 import java.io.BufferedReader;
 import java.io.IOException;
