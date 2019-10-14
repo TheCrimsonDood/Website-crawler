02b698cba486a78d086e0d0f9cbca345fdb338b9
Luca Bosse <luca.bosse+github@gmail.com>
2019-10-14 16:07:16 +0200

    added variable and getDateFromArticle halfways

--- src/dataminer/party.java
+++ src/dataminer/party.java
@@ -3,34 +3,63 @@ package src.dataminer;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Party {
     String name; // Name der Partei
-    int articleCount; // Anzahl der Artikel
+    int articleCount = 0; // Anzahl der Artikel
     HashMap<String, Integer> categorys = new HashMap<String, Integer>(); // Auflistung der Kategorien in denen die
                                                                          // Artikel veröffentlicht wurden mit Anzahl
     int beforeEU = 0;// Anzahl der Artikel, die vor der Eu-Wahl herausgebracht wurden
     int afterEU = 0; // ANzahl der Artikel, die nach der EU-Wahl herausgebracht wurden
+    int whileEU = 0; //Anzahl der Artikel, die während der EU-Wahl veröffentlicht wurden
     HashMap<String, Integer> releaseMonth = new HashMap<String, Integer>(); // Zählt die Anzahl der Artikel pro Monat
     int averageArticleLength;
     HashMap<String, Integer> authorCount = new HashMap<String, Integer>();
 
-    Party(String party) throws IOException, ParseException {
+    Party(String party,String path) throws IOException, ParseException {
         //---Variablen---
         this.name = party;
 
-        File folder = new File("src/data/" + name); // Erstellt den Ordnerpfad anhand des Namens des Objektes
+        File folder = new File(path + name); // Erstellt den Ordnerpfad anhand des Namens des Objektes
         File[] listOfFiles = folder.listFiles(); // Liest alle Datein im Ordner
+        int totalLength=0;//Gesamtlänge aller Artikel
 
 
         //---Funktionen---
         for (File file : listOfFiles) { // Schleife, die folgende Aktionen für jede Datei ausführen
             if (file.isFile()) {
-                Article article = new Article(folder+"/"+ file.getName());//Öffnet einen Artikel und ließt wichtige Daten aus
-                getAuthorFromArticle(article.author);//
+                Article article = new Article(folder+ file.getName());//Öffnet einen Artikel und ließt wichtige Daten aus
+                this.articleCount++; //Erhöht die Varaible um 1, da sie die Anzahl der Artikel wiedergibt
+                totalLength+=article.wordCount;
+                getAuthorFromArticle(article.author);//Ließt den Autor aus und speichert Ihn in der Variable authorCount
+                getDateFromArticle(article.date);
+
             }
         }
+        this.averageArticleLength = totalLength/this.articleCount;
+    }
+
+    private void getDateFromArticle(Date date) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date euStart = sdf.parse("2019-05-23 00:00:00");
+        Date euEnd = sdf.parse("2019-05-26 23:59:59");
+
+        if(date.after(euEnd)){
+            beforeEU++;
+        }else if(date.before(euStart)){
+            afterEU++;
+        }else{
+            whileEU++;
+        }
+        Calendar calendar;
+        Switch(date.getMonth()){
+
+        }
+        
     }
 
     private void getAuthorFromArticle(String author){
