f06116a19efb8d6b7a3bcd546ad07ec21c7d2446
Annercy <ann.kathrin.fulsche1999@gmail.com>
2019-10-18 10:28:32 +0200

    output fuction for dataminer

--- src/dataminer/dataminer.java
+++ src/dataminer/dataminer.java
@@ -2,10 +2,11 @@ package src.dataminer;
 
 import java.io.*;
 import java.text.ParseException;
+import java.util.HashMap;
 
 public class Dataminer {
 
-    public static void main(String[] args0) throws IOException, ParseException {
+    Dataminer() throws IOException, ParseException {
         
         File path = new File("src/data/");                                  //sets search path for party directories
         File[] parties = path.listFiles();
@@ -20,22 +21,54 @@ public class Dataminer {
             System.out.println("No directory found.");          
         } else{
             for (int i = 0; i<parties.length; i++ ){
+
+                int partyArticleCount = 0; // Anzahl der Artikel
+                HashMap<String, Integer> partyCategorys = new HashMap<String, Integer>(); // Auflistung der Kategorien in denen die
+                                                                                    // Artikel veröffentlicht wurden mit Anzahl
+                int partyBeforeEU = 0;// Anzahl der Artikel, die vor der Eu-Wahl herausgebracht wurden
+                int partyAfterEU = 0; // ANzahl der Artikel, die nach der EU-Wahl herausgebracht wurden
+                int partyWhileEU = 0; // Anzahl der Artikel, die während der EU-Wahl veröffentlicht wurden
+                int partyAverageArticleLength = 0;
+                HashMap<String, Integer> articleReleaseMonth = new HashMap<String, Integer>(); // Zählt die Anzahl der Artikel pro Monat
+                HashMap<String, Integer> articleKeywordCount = new HashMap<String, Integer>(); //Zählt die Verwendeten Tags/Keywords diefür die Artikel gesetzt wurden
+                HashMap<String, Integer> articleAuthorCount = new HashMap<String, Integer>();//Zählt die Anzahlder Artikel pro Autor
+                HashMap<String, Integer> articleExactWordCount = new HashMap<String, Integer>();//Zählt die in Artikeln verwendeten Wörter
+                HashMap<String, HashMap<String,Integer>> allKeywordRelations = new HashMap<String, HashMap<String, Integer>>();
+
                 File partynameFile = parties[i];
                 String partyname = partynameFile.toString();
-                partyname = partyname.substring(9);
+                partyname = partyname.substring(9);                         //extracts Party name from path
+                
                 System.out.println(partyname+" found.");                    //prints all found party names
-                //Party party = new Party(partyname, "src/data/");
+
+                Party party = new Party(partyname, "src/data/");            //creates new Party object
+                
+                partyArticleCount = party.articleCount;
+                partyCategorys = party.categorys;
+                partyBeforeEU = party.beforeEU;
+                partyAfterEU = party.afterEU;
+                partyWhileEU = party.whileEU;
+                partyAverageArticleLength = party.averageArticleLength;
+                articleReleaseMonth = party.releaseMonth;
+                articleKeywordCount = party.keywordCount;
+                articleAuthorCount = party.authorCount;
+                articleExactWordCount = party.exactWordCount;
+                allKeywordRelations = party.allKeywordRelations;
+
+                System.out.println(partyArticleCount);
+                System.out.println(partyCategorys);
+                System.out.println(partyBeforeEU);
+                System.out.println(partyAfterEU);
+                System.out.println(partyWhileEU);
+                System.out.println(partyAverageArticleLength);
+                System.out.println(articleReleaseMonth);
+                System.out.println(articleKeywordCount);
+                System.out.println(articleAuthorCount);
+                System.out.println(articleExactWordCount);
+                System.out.println(allKeywordRelations);
+
                 System.out.println("Completed mining for "+partyname);
             }
         }
     }
-
-    // public static void main(String[] args0) throws IOException, ParseException {
-
-    //     String path = "src/data/";
-    //     Party party = new Party("Piratenpartei", path);
-
-    //     System.out.println("stop");
-
-    // }
 }
---
+++ src/dataminer/main.java
@@ -0,0 +1,15 @@
+package src.dataminer;
+
+import java.io.IOException;
+import java.text.ParseException;
+import src.dataminer.Dataminer;;
+
+public class main {
+
+    public static void main (String[] args) throws IOException, ParseException {
+
+        Dataminer miner = new Dataminer();
+
+    }
+
+}
