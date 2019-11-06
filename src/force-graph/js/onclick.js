712f80863f0aa2e795244bc1296d5b61ce028fbc
TheCrimsonDood <luca.bosse+github@gmail.com>
2019-11-06 21:40:31 +0100

    aditional force-graph modification

--- src/force-graph/js/onclick.js
+++ src/force-graph/js/onclick.js
@@ -1,27 +1,26 @@
 const button = document.getElementById('button');
 const header = document.getElementById('mainHeader');
 const mainArea = document.getElementById('mainArea');
-const jsonFile = "../json/nodes.json";
+const jsonFile = "../json/test.json";
 const width = mainArea.offsetWidth;
-const height = 481;
+const height = mainArea.offsetHeight;
 
 
 // alert('test');
-$("#button").click(function() {
+$("#button").click(function () {
     if (!button.classList.contains('active')) {
         button.classList.add('active');
         // alert('hello');
 
-        $.getJSON(jsonFile, function(data) {
+        $.getJSON(jsonFile, function (data) {
             // data.nodes.forEach(node => {
             //     console.log(node);
             // });
-
+            var jsonFile;
             const Graph = ForceGraph()
 
-            Graph.cooldownTime(Infinity)
-                (document.getElementById('mainArea'))
-                .graphData(data)
+            (document.getElementById('mainArea'))
+            .graphData(jsonFile)
                 .nodeId('id')
                 .nodeVal('val')
                 .nodeLabel('id')
@@ -31,20 +30,30 @@ $("#button").click(function() {
                 .nodeRelSize(2)
                 .width(width)
                 .height(height)
-                .d3AlphaDecay(0.002)
+                .d3AlphaDecay(0.04)
                 .d3VelocityDecay(0.4)
                 .d3Force('collide', d3.forceCollide(Graph.nodeRelSize()))
-                .d3Force('box', () => {
-                    const SQUARE_HALF_SIDE = Graph.nodeRelSize() * N * 0.5;
-                    nodes.forEach(node => {
-                        const x = node.x || 0,
-                            y = node.y || 0;
-                        // bounce on box walls
-                        if (Math.abs(x) > SQUARE_HALF_SIDE) { node.vx *= -1; }
-                        if (Math.abs(y) > SQUARE_HALF_SIDE) { node.vy *= -1; }
-                    });
+            // .d3Force('box', () => {
+            //     const SQUARE_HALF_SIDE = Graph.nodeRelSize() * N * 0.5;
+            //     nodes.forEach(node => {
+            //         const x = node.x || 0,
+            //             y = node.y || 0;
+            //         // bounce on box walls
+            //         if (Math.abs(x) > SQUARE_HALF_SIDE) { node.vx *= -1; }
+            //         if (Math.abs(y) > SQUARE_HALF_SIDE) { node.vy *= -1; }
+            //     });
+            // })
+            // .backgroundColor("grey");
+            Graph.cooldownTime(Infinity)
+                .d3AlphaDecay(10)
+                .d3VelocityDecay(0)
+
+                data.nodes.forEach(node => {
+                const { nodes , links } = Graph.graphData();
+                Graph.graphData({
+                    nodes: [...nodes, {node]
                 })
-                // .backgroundColor("grey");
+                });
 
         });
 
